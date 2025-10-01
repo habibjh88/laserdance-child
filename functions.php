5f6dada1ff51af1c3019dc0f16643b25ed778152
Habib Rahaman <habibjh88@gmail.com>
2025-10-02 00:20:02 +0600

    CSS and gutenberg color

--- functions.php
+++ functions.php
@@ -34,3 +34,34 @@ function hello_elementor_child_scripts_styles() {
 
 }
 add_action( 'wp_enqueue_scripts', 'hello_elementor_child_scripts_styles', 20 );
+
+function laserdance_gutenberg_colors() {
+	add_theme_support('editor-color-palette', [
+		[
+			'name'  => __('Pink', 'hello-theme-child'),
+			'slug'  => 'pink',
+			'color' => '#FD38FA',
+		],
+		[
+			'name'  => __('Cyan', 'hello-theme-child'),
+			'slug'  => 'cyan',
+			'color' => '#02FFFF',
+		],
+		[
+			'name'  => __('Purple', 'hello-theme-child'),
+			'slug'  => 'purple',
+			'color' => '#9300FF',
+		],
+		[
+			'name'  => __('Red', 'hello-theme-child'),
+			'slug'  => 'red',
+			'color' => '#FF0032',
+		],
+		[
+			'name'  => __('Green', 'hello-theme-child'),
+			'slug'  => 'green',
+			'color' => '#65FF0B',
+		],
+	]);
+}
+add_action('after_setup_theme', 'laserdance_gutenberg_colors');
